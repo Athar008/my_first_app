5608c73efa3ced58580a274da20936030e0d4007
Athar Imtiyaz Zargar <imtiyazathar@gmail.com>
2023-04-17 15:56:23 +0530

    Updated

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -2,6 +2,6 @@ package com.nagra;
 
 public class MyApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!, This is Athar");
+        System.out.println("Hello world!, This is Athar. Nice to meet You");
     }
 }
