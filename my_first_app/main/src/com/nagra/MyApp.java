f8c9bd2ee72e3deb35298426fb95ab945cb6b3e7
Athar Imtiyaz Zargar <imtiyazathar@gmail.com>
2023-04-19 14:24:45 +0530

    Updated

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -1,7 +1,30 @@
 package com.nagra;
 
+
+import org.alcibiade.asciiart.coord.TextBoxSize;
+import org.alcibiade.asciiart.image.rasterize.ShapeRasterizer;
+import org.alcibiade.asciiart.raster.ExtensibleCharacterRaster;
+import org.alcibiade.asciiart.raster.Raster;
+import org.alcibiade.asciiart.raster.RasterContext;
+import org.alcibiade.asciiart.widget.PictureWidget;
+import org.alcibiade.asciiart.widget.TextWidget;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class MyApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!, This is Athar. Nice to meet You");
+        File image = new File("cartman.jpeg");
+        BufferedImage circleImage = ImageIO.read(image);
+
+        TextWidget widget = new PictureWidget(new TextBoxSize(80, 30),
+                circleImage, new ShapeRasterizer());
+        Raster raster = new ExtensibleCharacterRaster();
+
+        widget.render(new RasterContext(raster));
+        System.out.println(raster);
     }
 }
