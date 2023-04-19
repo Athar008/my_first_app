e8f82fbb8a678a030d3ec342512af6199a430e79
Athar Imtiyaz Zargar <imtiyazathar@gmail.com>
2023-04-19 16:11:05 +0530

    updated

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -9,22 +9,29 @@ import org.alcibiade.asciiart.raster.RasterContext;
 import org.alcibiade.asciiart.widget.PictureWidget;
 import org.alcibiade.asciiart.widget.TextWidget;
 
+import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class MyApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Hello world!, This is Athar. Nice to meet You");
-        File image = new File("cartman.jpeg");
-        BufferedImage circleImage = ImageIO.read(image);
+        try {
+            File image = new File("cartman.jpeg");
+            BufferedImage circleImage = ImageIO.read(image);
 
-        TextWidget widget = new PictureWidget(new TextBoxSize(80, 30),
-                circleImage, new ShapeRasterizer());
-        Raster raster = new ExtensibleCharacterRaster();
 
-        widget.render(new RasterContext(raster));
-        System.out.println(raster);
+            TextWidget widget = new PictureWidget(new TextBoxSize(80, 30),
+                    circleImage, new ShapeRasterizer());
+            Raster raster = new ExtensibleCharacterRaster();
+
+            widget.render(new RasterContext(raster));
+            System.out.println(raster);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
